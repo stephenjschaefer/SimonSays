f076aa4db5632e245813fa38af1175a71816cd1a
Steve Schaefer <stephen.schaefer@me.com>
2016-01-20 14:11:34 -0500

    Initial Commit

---
+++ src/SimonSays/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace SimonSays.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
