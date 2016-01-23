5781d597e3b60bbe66c8566e0d7f98e94a285c1f
Steve Schaefer <stephen.schaefer@me.com>
2016-01-23 11:49:21 -0500

    Added menu and instructions page.

--- src/SimonSays/Controllers/HomeController.cs
+++ src/SimonSays/Controllers/HomeController.cs
@@ -24,5 +24,10 @@ namespace SimonSays.Controllers
             ViewData["Title"] = "Start";
             return View("Index");
         }
+
+        public IActionResult Instructions()
+        {
+            return View();
+        }
     }
 }
